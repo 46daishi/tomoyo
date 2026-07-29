19cdc2d6ac109668c5034c902ed20e99472d2a2b
Shirogane Dai <dai46hana@gmail.com>
2026-07-29 10:56:01 +0200

    Timer element

---
+++ src/lib/components/Session.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { fly } from 'svelte/transition';
+    import { session } from '$lib/stores/session.svelte.js';
+</script>
+
+{#if session.running}
+    <span class="session-timer" transition:fly={{ y: -8, duration: 200 }}>
+        {session.formattedTime}
+    </span>
+{/if}
+
+<style>
+    .session-timer {
+        font-size: 0.75rem;
+        font-weight: 600;
+        color: var(--theme-textSecondary, #b3b3b3);
+        font-variant-numeric: tabular-nums;
+        text-align: center;
+    }
+</style>
